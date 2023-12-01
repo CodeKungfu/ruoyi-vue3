20941f8797dc4a97a3dfd1259284df1275cf26c2
keming <likeming@hqz.net.cn>
2023-12-01 16:43:48 +0800

    wip: update

--- vite.config.js
+++ vite.config.js
@@ -25,13 +25,13 @@ export default defineConfig(({ mode, command }) => {
     },
     // vite 相关配置
     server: {
-      port: 80,
+      port: 4080,
       host: true,
       open: true,
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: 'http://localhost:8080',
+          target: 'http://127.0.0.1:7071',
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, '')
         }
